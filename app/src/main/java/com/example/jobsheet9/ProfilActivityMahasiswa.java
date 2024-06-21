d8e7d1e6c332576db30c579a4854ac46cdb2ddca
Diannandaelisma14 <diannandaelisma7@gmail.com>
2024-06-21 10:51:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jobsheet9/ProfilActivityMahasiswa.java
@@ -0,0 +1,15 @@
+package com.example.jobsheet9;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilActivityMahasiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_mahasiswa);
+    }
+}
---
+++ app/src/main/java/com/example/jobsheet9/ProfilActivitySTMIK.java
@@ -0,0 +1,14 @@
+package com.example.jobsheet9;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilActivitySTMIK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_stmik);
+    }
+}
